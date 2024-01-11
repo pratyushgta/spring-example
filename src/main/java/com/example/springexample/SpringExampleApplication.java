e0d5f6fb0ccf8476b904b4a8bea135fffad76bde
Pratyush Kumar <pratyushgta@gmail.com>
2024-01-11 23:13:56 +0530

    Update #2 11-01-2024

--- src/main/java/com/example/springexample/SpringExampleApplication.java
+++ src/main/java/com/example/springexample/SpringExampleApplication.java
@@ -7,13 +7,54 @@
 
 package com.example.springexample;
 
+import com.example.springexample.entity.Address;
+import com.example.springexample.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 @SpringBootApplication
-public class SpringExampleApplication {
+public class SpringExampleApplication implements CommandLineRunner {
+    @Autowired
+    Employee ob4;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringExampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // MSA-E1-Q2
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        Employee ob = new Employee();
+        Address address = new Address();
+        System.out.println(">>> INPUT EMPLOYEE DETAILS <<<");
+        System.out.print("ID: ");
+        ob.setId(Integer.parseInt(br.readLine()));
+
+        System.out.print("Name: ");
+        ob.setName(br.readLine());
+
+        System.out.print("City: ");
+        address.setCity(br.readLine());
+
+        System.out.print("State: ");
+        address.setState(br.readLine());
+
+        System.out.print("Pin: ");
+        address.setPin(Long.parseLong(br.readLine()));
+
+        ob.setAddress(address);
+        System.out.println(ob);
+
+        // MSA-E1-Q4
+        System.out.println(ob4);
     }
 }
--- src/main/java/com/example/springexample/controller/MyController.java
+++ src/main/java/com/example/springexample/controller/MyController.java
@@ -6,12 +6,11 @@
  */
 package com.example.springexample.controller;
 
+import com.example.springexample.SpringExampleApplication;
 import com.example.springexample.entity.Address;
 import com.example.springexample.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyController {
@@ -21,7 +20,8 @@ public class MyController {
     }
 
     // MSA-E1-Q1
-    Employee ob1 = new Employee();
+    @Autowired
+    Employee ob1;
 
     @RequestMapping("/details1")
     String employee_details1() {
@@ -36,21 +36,14 @@ public class MyController {
         return "<h4 style=\"font-family:verdana; color:teal;\"> Employee details: " + ob1;
     }
 
-    // MSA-E1-Q2
+    // MSA-E1-Q2 NOT WORKING
     @Autowired
     Employee ob2;
 
-    @RequestMapping("/details2")
-    String employee_details() {
-        ob2.setId(101);
-        ob2.setName("Mr Bean");
-
-        Address address = new Address();
-        address.setCity("Mumbai");
-        address.setState("MH");
-        address.setPin(400076);
-        ob2.setAddress(address);
-        return "<h4 style=\"font-family:verdana; color:lightcoral;\"> Employee details: " + ob2;
+    @PostMapping("/details2")
+    public String initEmployee(@RequestBody Employee employee) {
+        this.ob2 = employee;
+        return "Object2 initialised!";
     }
 
     // MSA-E1-Q3
@@ -59,8 +52,37 @@ public class MyController {
 
     @RequestMapping("/details3")
     String employee_details3() {
-        ob3.setId(101);
+        ob3.setId(103);
         ob3.setName("Mr Bean");
-        return "<h4 style=\"font-family:verdana; color:teal;\"> Employee details: " + ob3;
+        return "<h4 style=\"font-family:verdana; color:lightcoral;\"> Employee details: " + ob3;
+    }
+
+    // MSA-E1-Q4
+    @Autowired
+    Employee ob4;
+
+    @RequestMapping("/details4")
+    String employee_details4() {
+        ob4.setId(104);
+        ob4.setName("Mr Bean");
+        return "<h4 style=\"font-family:verdana; color:navy;\"> Employee details: " + ob4;
+    }
+
+    // MSA-E1-Q5
+    Employee ob5 = new Employee();
+
+    @RequestMapping("/details5")
+    String employee_details5() {
+        ob5.setId(105);
+        ob5.setName("Mr Bean");
+
+        Address address = new Address();
+        address.setCity("Mumbai");
+        address.setState("MH");
+        address.setPin(400076);
+        ob5.setAddress(address);
+
+        System.out.println(ob5);
+        return "<h4 style=\"font-family:verdana; color:darkgoldenrod;\"> Employee details: " + ob5;
     }
 }
